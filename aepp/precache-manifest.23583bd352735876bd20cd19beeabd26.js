716866c586038e5d2242305ecc2976fd3d680e83
emmanuelJet <emmanueljet774@gmail.com>
2020-04-11 02:38:43 +0100

    Test

---
+++ aepp/precache-manifest.23583bd352735876bd20cd19beeabd26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105d6486f3b3bb14385eacf7e7ad63dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469db3452d9aa8fa1f93",
+    "url": "/static/css/main.4c06968d.chunk.css"
+  },
+  {
+    "revision": "37d7d68a70002579d2f9",
+    "url": "/static/js/2.843a62ea.chunk.js"
+  },
+  {
+    "revision": "469db3452d9aa8fa1f93",
+    "url": "/static/js/main.b4f447b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
