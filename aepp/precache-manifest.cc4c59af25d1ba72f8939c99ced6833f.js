e6d18275e80ac7143ad3f86257c3eb88adda85f4
emmanuelJet <emmanueljet774@gmail.com>
2020-04-11 02:53:34 +0100

    Test Debug

---
+++ aepp/precache-manifest.cc4c59af25d1ba72f8939c99ced6833f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8638f0ceb92ddc48342cac41daa729f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c0b7410e169c3e6161d",
+    "url": "/static/css/main.4c06968d.chunk.css"
+  },
+  {
+    "revision": "735c56800c5d3465c4d5",
+    "url": "/static/js/2.70196fa1.chunk.js"
+  },
+  {
+    "revision": "1c0b7410e169c3e6161d",
+    "url": "/static/js/main.e8a6426e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- aepp/service-worker.js
+++ aepp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23583bd352735876bd20cd19beeabd26.js"
+  "/precache-manifest.cc4c59af25d1ba72f8939c99ced6833f.js"
 );
 
 self.addEventListener('message', (event) => {
